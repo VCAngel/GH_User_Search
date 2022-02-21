32155610f572b7a065aaa616ad24699b11094604
VCAngel <vargascasavantesangel.329@gmail.com>
2022-02-20 23:17:21 -0700

    App runs now! Components todo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './ReportWebVitals';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <h1>Hello world!</h1>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+reportWebVitals();
